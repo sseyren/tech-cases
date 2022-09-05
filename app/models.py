05226b449c5d5d844d0d6ec11b6c7373742b8478
Serhat Seyren <thesseyren@gmail.com>
2022-09-05 21:37:45 +0300

    remove CompanyType enum from class scope

--- app/models.py
+++ app/models.py
@@ -5,13 +5,14 @@ from django.utils.translation import gettext_lazy as _
 from django_countries.fields import CountryField
 
 
-class Company(models.Model):
-    class CompanyType(models.TextChoices):
-        SOLE = "sole", _('Sole Proprietorship')  # Şahıs Şirketi
-        SME = "sme", _('Small and Medium-Sized Enterprise')  # KOBİ
-        CORP = "corp", _('Corporation')  # Büyük İşletme
-        SCO = "cso", _('Civil Society Organization')  # STK
+class CompanyType(models.TextChoices):
+    SOLE = "sole", _('Sole Proprietorship')  # Şahıs Şirketi
+    SME = "sme", _('Small and Medium-Sized Enterprise')  # KOBİ
+    CORP = "corp", _('Corporation')  # Büyük İşletme
+    SCO = "cso", _('Civil Society Organization')  # STK
 
+
+class Company(models.Model):
     name = models.CharField(max_length=100)
     image = models.ImageField(null=True, blank=True)
     type = models.CharField(max_length=5, choices=CompanyType.choices)
