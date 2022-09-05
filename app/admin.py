335b9738d64e1c740cfbcc765b2f297570b940d5
Serhat Seyren <thesseyren@gmail.com>
2022-09-05 12:58:17 +0300

    add Company model

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Company
+
+
+class CompanyAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Company, CompanyAdmin)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from django_countries.fields import CountryField
+
+
+class Company(models.Model):
+    class CompanyType(models.TextChoices):
+        SOLE = "sole", _('Sole Proprietorship')  # Şahıs Şirketi
+        SME = "sme", _('Small and Medium-Sized Enterprise')  # KOBİ
+        CORP = "corp", _('Corporation')  # Büyük İşletme
+        SCO = "cso", _('Civil Society Organization')  # STK
+
+    name = models.CharField(max_length=100)
+    image = models.ImageField(null=True, blank=True)
+    type = models.CharField(max_length=5, choices=CompanyType.choices)
+    country = CountryField()
+    url = models.URLField(blank=True)
+    employee_count = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.name} ({self.id})"
